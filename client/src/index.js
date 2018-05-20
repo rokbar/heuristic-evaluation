6e559aaaf3a71c24ab70e4cd827a0cb211e73403
rokbar <r.barsauskas@gmail.com>
2018-05-20 10:56:39 +0300

    Redux DevTools setup.

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
+import {Provider} from 'react-redux';
+import {ConnectedRouter, routerMiddleware} from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 
 import './index.css';
@@ -11,25 +11,28 @@ import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers';
 import Routes from './routes';
-import { INIT } from './actions/types';
+import {INIT} from './actions/types';
 import localStorageLoad from './middleware/localStorageLoad';
 
 const history = createHistory();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // localStorageLoad - middleware to authenticate on app load (if token exists)
-const createStoreWithMiddleware = applyMiddleware(
-  routerMiddleware(history),
-  thunk,
-  localStorageLoad,
-)(createStore);
-const store = createStoreWithMiddleware(reducers);
+const enhancer = composeEnhancers(
+  applyMiddleware(
+    routerMiddleware(history),
+    thunk,
+    localStorageLoad,
+  )
+);
+const store = createStore(reducers, enhancer);
 
 // dispatch an initialization action before app renders
-store.dispatch({ type: INIT });
+store.dispatch({type: INIT});
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Routes />
+      <Routes/>
     </ConnectedRouter>
   </Provider>
   , document.getElementById('root')
